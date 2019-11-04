39c39a7f2516c24163e03ead521523699ba6c646
Alvin <alvinpl21@gmail.com>
2019-11-03 23:38:47 -0500

    revised portfolio

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 
-function pressHome(){
-    $('#buttonH').click(function(e){
-        $('.homeP').toggle('slow');
-        $('#buttonB').toggle();
-    })
-}
+// function pressHome(){
+//     $('#buttonH').click(function(e){
+//         $('.homeP').toggle('slow');
+//         $('#buttonB').toggle();
+//     })
+// }
 
-function pressBio(){
-    $('#buttonB').click(function(e) {
-        $('.bioP').toggle('slow');
-        $('#buttonP').toggle();
-    })
-}
+// function pressBio(){
+//     $('#buttonB').click(function(e) {
+//         $('.bioP').toggle('slow');
+//         $('#buttonP').toggle();
+//     })
+// }
 
-function pressProjects() {
-    $('#buttonP').click(function (e) {
-        $('.projectsP').toggle('slow');
-    })
-}
+// function pressProjects() {
+//     $('#buttonP').click(function (e) {
+//         $('.projectsP').toggle('slow');
+//     })
+// }
 
 
-function portLoaded(){
-    pressHome();
-    pressBio();
-    pressProjects();
-}
-$(portLoaded);
+// function portLoaded(){
+//     pressHome();
+//     pressBio();
+//     pressProjects();
+// }
+// $(portLoaded);
